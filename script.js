c214203df5b3410d70edd5ce65c23cce7ec0bcde
Shayan Ahmed <shayanahmed7454@gmail.com>
2024-11-12 02:14:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Digital Marketer', 'Forex Trader'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
