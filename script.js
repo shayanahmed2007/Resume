0764ed2bc256d829e13926c6b0128f8ebcca4ef0
Shayan Ahmed <shayanahmed7454@gmail.com>
2024-11-15 04:09:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Website Developer', 'Software Developer', 'WordPress Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+alert("Before you consider hiring me, make sure to take a quick look at my skills and experience!")
