d018829b6b3a0c750677aec2b19fc481e7e406fb
Shayan Ahmed <shayanahmed7454@gmail.com>
2024-11-15 04:02:59 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Digital Marketer', 'Forex Trader'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
